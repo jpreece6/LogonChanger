5815b448f66b0dee014bf21be6c1ceb2ea0a3f01
Josh Preece <looterwar@gmail.com>
2016-07-27 19:36:21 +0100

    Updated Util
    Implemented timestamp functionality

--- LogonChanger/Util.cs
+++ LogonChanger/Util.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -33,7 +34,7 @@ namespace LogonChanger
 
         public static string GenerateFileTimeStamp()
         {
-            return "";
+            return DateTime.Now.ToString("yyyyMMdd-HHmm");
         }
     }
 }
