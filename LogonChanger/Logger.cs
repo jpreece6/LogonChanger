7dd17838ae5786a288ee089de0d0440608621ba9
Josh Preece <looterwar@gmail.com>
2016-07-27 19:37:05 +0100

    Added Logger
    Logger is to be used as the method of communication between the service
    and preferred output (EventLog)

---
+++ LogonChanger/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogonChanger
+{
+    public static class Logger
+    {
+        public static bool Verbose { get; set; }
+
+        public static void WriteInformation(string message)
+        {
+            if (Verbose)
+                EventLog.WriteEntry("LogonChanger", message, EventLogEntryType.Information);
+        }
+
+        public static void WriteError(string message, Exception e)
+        {
+            EventLog.WriteEntry("LogonChanger", message + "\n\n" + e.Message);
+        }
+    }
+}
