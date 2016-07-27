467ab49923f5018a0e496b6a79eedc615784ffa1
Josh Preece <looterwar@gmail.com>
2016-07-27 19:35:47 +0100

    Added test harness
    New console based test harness to aid in development

---
+++ TestHarness/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LogonChanger;
+using SettingsVault;
+
+namespace TestHarness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var changer = new Changer();
+
+            Settings.Init(Config.SettingsFilePath);
+
+            Settings.Default.Set(Config.Interval, 3600000); // 1 hour
+            Settings.Default.Set(Config.BingXmlkey, "urlBase");
+            Settings.Default.Set(Config.Resolution, "1920x1200");
+            Settings.Default.Set(Config.WallpaperDir, @"C:\LogonWallpapers\");
+            Settings.Default.Set(Config.Url, @"http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=en-GB");
+            Settings.Default.Set(Config.Verbose, false);
+
+            Settings.Default.Save();
+
+            changer.Update();
+        }
+    }
+}
