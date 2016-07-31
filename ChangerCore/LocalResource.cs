cfb061b458c91dd968b5db379c2c0361ceef1658
Josh Preece <looterwar@gmail.com>
2016-07-31 15:13:51 +0100

    Updated
    Local resources can now be shown in shuffle mode or sequence mode

--- ChangerCore/LocalResource.cs
+++ ChangerCore/LocalResource.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
 using System.IO;
+using System.Linq;
+using SettingsVault;
 
 namespace ChangerCore
 {
@@ -16,10 +20,34 @@ namespace ChangerCore
         public string GetResource(string folderPath)
         {
             EnumerateFiles(folderPath);
-            Random rnd = new Random();
-            int randIndex = rnd.Next(0, _enumeratedFiles.Length);
 
-            return _enumeratedFiles[randIndex];
+            if (_enumeratedFiles.Length == 0)
+                return null;
+
+            var shuffle = Settings.Default.Get<bool>(Config.Shuffle, false);
+
+            if (shuffle)
+            {
+
+                Random rnd = new Random();
+                int randIndex = rnd.Next(0, _enumeratedFiles.Length);
+
+                return _enumeratedFiles[randIndex];
+
+            }
+
+            var fileIndex = Settings.Default.Get<int>(Config.FileIndex, 0);
+
+            // If larger than our bounds reset to 0
+            if (fileIndex > _enumeratedFiles.Length)
+                fileIndex = 0;
+
+            var selectedFile = _enumeratedFiles[fileIndex];
+
+            Settings.Default.Set(Config.FileIndex, ++fileIndex);
+            Settings.Default.Save();
+
+            return selectedFile;
         }
 
         public string GetResourceFromConfig(string configPath)
